2a0a00d9c8866d576d817ccb9af2ed36554e80e6
igitit1 <sidnshady@gmail.com>
2018-06-06 04:20:28 +0530

    turned into hacker news

---
+++ app.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('myAppController', function($scope, $http){
+	$scope.posts = [];
+
+	$http.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then(function(response){
+		$scope.top_stories = response.data;
+		for(var i=0;i<$scope.top_stories.length;++i){
+			$http.get('https://hacker-news.firebaseio.com/v0/item/' + $scope.top_stories[i] + '.json?print=pretty').then(function(response){
+				$scope.posts.push(response.data);
+			});
+		}
+	});
+});
